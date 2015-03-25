3758d88f2cd8388886167cd54ca3d2348d82bfc5
Brad Gill <bradgill@gmail.com>
2015-03-24 20:48:23 -0700

    tabs style changes
    string length and it now skips h2.subtitle

--- assets/tabs.js
+++ assets/tabs.js
@@ -1,9 +1,10 @@
 var Scroller = (function() {
+	
 
 	var scroller = {};
 	
 	var mobile_cutoff = 585;
-	var topbar_offset = 65;
+	var topbar_offset = 80;
 	
 	scroller.recalc = function(e) {
 		var container = $('#submenu');
@@ -14,7 +15,10 @@ var Scroller = (function() {
 		} else {
 			container.width( 'auto' );	
 		}
-			
+		if ( $(window).width() <= mobile_cutoff ) {
+			$('div.article-cover').attr('style', 'margin-top: ' + $(container).height() + 'px;');
+		} else
+			$('div.article-cover').attr('style', '');
 
 		
 		var offset = $(window).scrollTop();
@@ -23,7 +27,7 @@ var Scroller = (function() {
 		else 
 			container.removeClass('fixed');
 		var height = $(window).height();
-
+		
 		var middle = offset + height/2;
 		var blocks = [];
 		
@@ -47,19 +51,15 @@ var Scroller = (function() {
 		container.find('li.current').removeClass();
 		for( var i=0; i < blocks.length; i++ ) {
 			if ( middle > blocks[i].from && middle < blocks[i].to ) {
-				console.log('in the middle of ' + blocks[i].anchor);
 				container.find('li a[href=#' + blocks[i].anchor + ']').parent().addClass('current');
 			}
 		}
-
-
-		console.debug( blocks );
 	}
 
 	scroller.sanitize = function( text ) {
 		return text.replace(/\s+/g, '-').replace(/[^0-9a-z\-]/ig, '');
 	}
-
+	
 	scroller.scrollToAnchor = function(e) {
 		var a = $(e.currentTarget);
 		var target = $('a[name=' + a.attr('href').substr(1) + ']');
@@ -70,29 +70,28 @@ var Scroller = (function() {
 		$('body').animate( {scrollTop: target.offset().top - offset }, 'slow' );
 	//	return false;
 	}
-
+	
 	return scroller;
 })();
 
 
 $(document).ready( function() {
-	var out = '<ul id="submenu">';
+	var out = '<div id="submenu"><ul>';
 	var amap = {};
-	$('h1,h2').each( function(i) { //sections
+	$('h1,h2:not(.subtitle)').each( function(i) { //sections
 		var anchor = Scroller.sanitize($(this).text());
-		if ( amap.hasOwnProperty(anchor) )
+		if ( amap.hasOwnProperty(anchor) ) 
 			anchor += i;
-
+		
 		amap[anchor] = true;
 		var caption = $(this).text();
-		var cutoff = 32;
-		if ( caption.length > cutoff )
-			caption = caption.substr(0,cutoff-3) + '...';
 		$(this).prepend('<a name="'+anchor+'"></a>');
 		out += '<li><a href="#'+anchor+'">'+caption+'</a></li>';
 	});
-	out += '</ul>';
+	out += '</ul><div class="clr">.</div></div>';
 	$('section.site-nav').after( out );
 	$('#submenu a').click( Scroller.scrollToAnchor );
 	$(window).scroll( Scroller.recalc ).resize( Scroller.recalc );
 });
+
+
